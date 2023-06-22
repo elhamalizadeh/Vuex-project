73511b06d80d6dc1b8f988b8f187bac4a28a9203
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2023-06-22 11:04:41 +0330

    2-create header and homePage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<Header />
+<router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import router from './router.js';
+import store from './store/index'
 
-createApp(App).mount('#app')
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+//import 'bootstrap-icons/font/bootstrap-icons.css'
+
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount('#app');
