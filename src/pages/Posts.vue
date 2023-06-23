c7705b14ad46bda6cdb9571d6c056fdd31fcf69e
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2023-06-23 14:28:22 +0330

    pages

---
+++ src/pages/Posts.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>Posts List</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <form class="form-control">
+          <label>Title:</label>
+          <input type="text" class="form-control"/>
+           <label>Description:</label>
+          <textarea type="text" class="form-control"></textarea>
+          <button type="submit" class="btn btn-success mt-3">create</button>
+        </form>
+      </div>
+      <hr/>
+      <div class="col-md-9">
+        <table class="table">
+          <tbody >
+            <tr>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+            <tr>
+              <td>first</td>
+              <td>second description</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Posts-Page',
+
+    setup:{
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import {createRouter, createWebHistory} from 'vue-router' ;
-import HomePage from './pages/Home.vue'
+import HomePage from './pages/Home.vue';
+import PostsList from './pages/Posts.vue'
 
-const routes =[{path:"/", name :"Home-Page" , component: HomePage}];
+const routes =[{path:"/", name :"Home-Page" , component: HomePage},
+{path:"/posts", name :"Posts-Page" , component: PostsList}];
 
 const router = createRouter({
     history:createWebHistory(),
